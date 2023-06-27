2cd54a843b1e5297c8ca5a086caa5c5f9e300305
nicole-nam <eunjungnam@gmail.com>
2023-06-27 13:41:27 -0400

    Added handle click to get rid of contact info at the top on click

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,45 @@
+import React, { useState } from "react";
 import Navigation from "./Navigation";
 
-const Header = () =>{
-    return(
-        <header>
+const Header = () => {
+  const [showHeader, setShowHeader] = useState(true);
 
-            <div className="headerContainer">
-                <div className="headerContent">
-                    <div className="headerText">
-                        <p>You can now text our store at 
-                            <a className="">(437)-747-8473</a>
-                        </p>
-                    </div>  
-                </div>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="sc-bIyKgX iLWVze"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+  const handleClick = () => {
+    setShowHeader(false);
+  };
+
+  return (
+    <header>
+      {showHeader && (
+        <div className="headerContainer">
+          <div className="headerContent">
+            <div className="headerText">
+              <p>
+                You can now text our store at
+                <a className="">(437)-747-8473</a>
+              </p>
             </div>
-
-        </header>
-    )
-}
+          </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#FFFFFF"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="sc-bIyKgX iLWVze"
+            onClick={handleClick}
+          >
+            <line x1="18" y1="6" x2="6" y2="18"></line>
+            <line x1="6" y1="6" x2="18" y2="18"></line>
+          </svg>
+        </div>
+      )}
+    </header>
+  );
+};
 
 export default Header;
