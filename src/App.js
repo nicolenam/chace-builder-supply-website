a43ded6ad8daf2fe6be23a60fe3ddb9b49d0c46e
nicole-nam <eunjungnam@gmail.com>
2023-06-27 21:42:11 -0400

    Added hero section and initial popular section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Header from './components/Header';
+import Hero from './components/Hero';
+import PopularProducts from './components/PopularProducts';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main>
+        <Hero />
+        <PopularProducts />
+      </main>
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,22 @@
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="heroImage">
+        <div className="heroContainer">
+          <div className="heroContext">
+            <h2 className="heroTitle">Looking for a quote?</h2>
+            <p className="heroP">Easily submit a quote request for materials online, 24/7!</p>
+            <div className="quotesDiv"></div>
+            <a className="quotesBtn" href="/quotes">
+              <div className="quotesBtnDiv">
+                <p className="btnText">Get a Quote</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
