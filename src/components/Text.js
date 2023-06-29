fecb180db7349b8e314498aaa52cbb8bdb1b07ff
nicole-nam <eunjungnam@gmail.com>
2023-06-29 11:46:52 -0400

    Clean up heading

--- src/components/Text.js
+++ src/components/Text.js
@@ -6,7 +6,7 @@ const Text = () =>{
 
             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>
 
-            <h5 className="textUs">Text Us</h5>
+            <h4 className="textUs">Text Us</h4>
         </button>
     )
 }
