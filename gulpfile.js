d01cac965fa4ba7e20ba889f3f0069bb580b0988
nicole-nam <eunjungnam@gmail.com>
2023-06-30 23:44:30 -0400

    changed path

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,8 @@ const cleanCSS = require('gulp-clean-css');
 
 // Task to concatenate and minify JavaScript files
 gulp.task('js', function () {
-  return gulp.src('src/components/*.js') // Specify the source path for your JavaScript files
+    // src/components/*.js
+  return gulp.src('./src/components/*.js') // Specify the source path for your JavaScript files
     .pipe(concat('bundle.js')) // Concatenate all JavaScript files into a single file named 'bundle.js'
     .pipe(uglify()) // Minify the JavaScript code
     .pipe(gulp.dest('dist/js')); // Specify the destination path for the minified JavaScript file
@@ -15,7 +16,8 @@ gulp.task('js', function () {
 
 // Task to concatenate and minify CSS files
 gulp.task('css', function () {
-    return gulp.src('src/styles/*.scss') // Specify the source path for your Sass files
+    // src/styles/*.scss
+    return gulp.src('./src/styles/*.scss') // Specify the source path for your Sass files
     .pipe(sass().on('error', sass.logError)) // Compile Sass to CSS
     .pipe(concat('styles.css')) // Concatenate all CSS files into a single file named 'styles.css'
     .pipe(cleanCSS()) // Minify the CSS code
