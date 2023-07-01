00a4c8d81f6715fbd692e87ca7eeaca3272d6a7d
nicole-nam <eunjungnam@gmail.com>
2023-06-30 20:37:19 -0400

    Added css for product section

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,19 +1,9 @@
-import { useState } from 'react';
 import "../styles/Gallery.scss";
+import Item from "./Item";
 
 const PopularProducts = () => {
 
-    const [isHovered, setIsHovered] = useState(false);
 
-    const handleMouseEnter = () => {
-      setIsHovered(true);
-      console.log("enter");
-    };
-  
-    const handleMouseLeave = () => {
-      setIsHovered(false);
-      console.log("leave");
-    };
 
     const products = [
       {
@@ -90,40 +80,12 @@ const PopularProducts = () => {
       <div  className="scrollmenu">
 
         {products.map((product) => (
-          <div key={product} className="row">
-            <img src={product.url} alt={product.sku}/>
-            <p>{product.measurement}</p>
-            <p>sku: {product.sku}</p>
-            <p>{product.price}</p>
-
-
-
-
-      <div className="sc-gsTCUz sc-hKgILt sc-gATInX bhdLno gTLZXx jHlPUB">
-
-          <div  className={`sc-gsTCUz sc-hKgILt sc-jfJzZe sc-jcRDWI sc-fkubWd bhdLno gTLZXx gdsrAv kPwfsx iXQHrd ${isHovered ? 'hovered' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-            <svg xmlns="http://www.w3.org/2000/svg" style={{ width: '24px', height: '24px' }} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="sc-eCjkpP iDHATf">
-              <line x1="12" y1="5" x2="12" y2="19"></line>
-              <line x1="5" y1="12" x2="19" y2="12"></line>
-            </svg>
-            {
-              isHovered ?
-              <p className="sc-hOqqkJ cWGOwq show">Add To Cart</p>
-              :
-              <p className="sc-hOqqkJ cWGOwq show">Add</p>
-            }
-            </div>
-          </div>
-        </div>
-
+          
+          <Item key={product} product={product}/>
         
         ))}
 
       </div>
-
-      {/* button */}
-
-
 
     </section>
   );
---
+++ src/components/Item.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import "../styles/Gallery.scss";
+
+const Item = ({product}) =>{
+
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+      setIsHovered(true);
+      console.log("enter");
+    };
+  
+    const handleMouseLeave = () => {
+      setIsHovered(false);
+      console.log("leave");
+    };
+
+    return (
+        <div key={product} className="row">
+            <img src={product.url} alt={product.sku}/>
+            <div className="details">
+                <div>
+                    <p className="measurement">{product.measurement}</p>
+                    <p className="sku">sku: {product.sku}</p>
+                </div>
+                <div>
+                    <p className="price">{product.price}</p>
+                </div>
+            </div>
+            <div className="sc-gsTCUz sc-hKgILt sc-gATInX bhdLno gTLZXx jHlPUB">
+
+                <div  className={`sc-gsTCUz sc-hKgILt sc-jfJzZe sc-jcRDWI sc-fkubWd bhdLno gTLZXx gdsrAv kPwfsx iXQHrd ${isHovered ? 'hovered' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                  <svg xmlns="http://www.w3.org/2000/svg" style={{ width: '24px', height: '24px' }} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="sc-eCjkpP iDHATf">
+                    <line x1="12" y1="5" x2="12" y2="19"></line>
+                    <line x1="5" y1="12" x2="19" y2="12"></line>
+                  </svg>
+                  {
+                    isHovered ?
+                    <p className="sc-hOqqkJ cWGOwq show">Add To Cart</p>
+                    :
+                    <p className="sc-hOqqkJ cWGOwq show">Add</p>
+                  }
+                  </div>
+                </div>
+              </div>
+    )
+}
+
+export default Item;
