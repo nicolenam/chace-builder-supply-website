f78a0a0469776e63fb68cff22873c407390e1d0d
nicole-nam <eunjungnam@gmail.com>
2023-06-30 23:42:31 -0400

    updated the path

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const cleanCSS = require('gulp-clean-css');
-const sass = require('gulp-sass')(require('sass'));
 
 
 // Task to concatenate and minify JavaScript files
 gulp.task('js', function () {
-  return gulp.src('src/js/*.js') // Specify the source path for your JavaScript files
+  return gulp.src('src/components/*.js') // Specify the source path for your JavaScript files
     .pipe(concat('bundle.js')) // Concatenate all JavaScript files into a single file named 'bundle.js'
     .pipe(uglify()) // Minify the JavaScript code
     .pipe(gulp.dest('dist/js')); // Specify the destination path for the minified JavaScript file
@@ -15,7 +15,7 @@ gulp.task('js', function () {
 
 // Task to concatenate and minify CSS files
 gulp.task('css', function () {
-    return gulp.src('src/scss/*.scss') // Specify the source path for your Sass files
+    return gulp.src('src/styles/*.scss') // Specify the source path for your Sass files
     .pipe(sass().on('error', sass.logError)) // Compile Sass to CSS
     .pipe(concat('styles.css')) // Concatenate all CSS files into a single file named 'styles.css'
     .pipe(cleanCSS()) // Minify the CSS code
